dc6616a5c7c29051ec7b8263b1293115e4000c18
HL1115 <1127570788@qq.com>
2019-04-16 10:05:47 +0800

    react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './index.css';
 import './index.less';
 import num from './home';
-let fn = ()=>{
-    console.log(num+11);
-}
-fn();
-fn();
-class Person{
-    say = ()=>{
-        alert('hello');
-    }
-}
-let p1 = new Person();
-p1.say();
+
+let ele = <h1>{num}</h1>;
+ReactDOM.render(
+    ele,
+    document.querySelector('#root')
+)
+
+// let fn = ()=>{
+//     console.log(num+11);
+// }
+// fn();
+// fn();
+// class Person{
+//     say = ()=>{
+//         alert('hello');
+//     }
+// }
+// let p1 = new Person();
+// p1.say();
--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
                 use: {
                   loader: 'babel-loader',
                   options: {
-                    presets: ['@babel/preset-env'],
+                    presets: ['@babel/preset-env',"@babel/preset-react"],
                     "plugins": [
                         "@babel/plugin-transform-runtime",
                         "@babel/plugin-proposal-class-properties"
