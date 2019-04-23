3f0abcabe419bf14510bddd07b807efd94f73b91
HL1115 <1127570788@qq.com>
2019-04-23 08:41:25 +0800

    search

--- src/containers/Home/subComponents/banner/index.js
+++ src/containers/Home/subComponents/banner/index.js
@@ -4,7 +4,14 @@ export default class Banner extends Component {
     render() {
         return (
             <div className="banner">
-            
+                <p className="tit">悠游古欧洲   遍览德法瑞意  </p>
+                <p className="tit">全程升级热早+1晚5星酒店</p>
+                <div className="search">
+                    <label>
+                        <i className="iconfont icon-weizhi"></i>
+                        <span>目的地</span>    
+                    </label>
+                </div>
             </div>
         )
     }
