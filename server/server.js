db440ef7e532381942a9bc9556a4313826bdea3a
HL1115 <1127570788@qq.com>
2019-04-23 10:40:18 +0800

    axios

--- server/server.js
+++ server/server.js
@@ -4,9 +4,29 @@ let app = express();
 app.listen(8080);
 let list = [
     {
-        img:,
+        img:'https://raw.githubusercontent.com/HL1115/webpack/master/src/assets/imgs/list1.png',
         text:'目的地'
     },
+    {
+        img:'https://raw.githubusercontent.com/HL1115/webpack/master/src/assets/imgs/list1.png',
+        text:'亲子游学'
+    },
+    {
+        img:'https://raw.githubusercontent.com/HL1115/webpack/master/src/assets/imgs/list1.png',
+        text:'酒店预订'
+    },
+    {
+        img:'https://raw.githubusercontent.com/HL1115/webpack/master/src/assets/imgs/list1.png',
+        text:'车辆接送'
+    },
+    {
+        img:'https://raw.githubusercontent.com/HL1115/webpack/master/src/assets/imgs/list1.png',
+        text:'签证代办'
+    },
+    {
+        img:'https://raw.githubusercontent.com/HL1115/webpack/master/src/assets/imgs/list1.png',
+        text:'精彩瞬间'
+    }
 ]
 
 app.get('/api/list',(req,res)=>{
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import Loading from '../components/Loading';
+import axios from 'axios';
 export default class App extends Component {
     constructor() {
         super();
@@ -10,6 +11,9 @@ export default class App extends Component {
         }
     }
     componentDidMount() {
+        axios.get('/api/list').then((data)=>{
+            console.log(data.data);
+        })
         setTimeout(() => {
             this.setState({
                 isLoad:true
