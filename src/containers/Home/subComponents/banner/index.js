1fe4827b57253aeb3d6f2d27c99f7c3f58b76d2b
HL1115 <1127570788@qq.com>
2019-04-23 09:08:15 +0800

    受控组件

--- src/containers/Home/subComponents/banner/index.js
+++ src/containers/Home/subComponents/banner/index.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
 import './index.less';
 export default class Banner extends Component {
+    constructor(){
+        super();
+        this.state = {
+            inputValue:''
+        }
+    }
+    handleChange = (e)=>{
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
+    handleSearch = ()=>{
+        // 受控组件
+        console.log(this.state.inputValue);
+
+
+        // 非受控组件
+        // console.log(this.inp.value)
+        // axios.get('?data='+this.inp.value).then()
+    }
+    
     render() {
         return (
             <div className="banner">
@@ -12,9 +33,9 @@ export default class Banner extends Component {
                         <span>目的地</span>    
                     </label>
                     <div>
-                        <input type="text" placeholder="日本 美国 英国 地中海"/>
+                        <input onChange={this.handleChange} value={this.state.inputValue} ref={(inp)=>this.inp = inp} type="text" placeholder="日本 美国 英国 地中海"/>
                     </div>
-                    <button>
+                    <button onClick={this.handleSearch}>
                         <i className="iconfont icon-search"></i>
                     </button>
                 </div>
