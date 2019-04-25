04c11783f099cd05cfacbbbacf218f2057858b73
HL1115 <1127570788@qq.com>
2019-04-25 09:32:42 +0800

    jiekou

--- server/server.js
+++ server/server.js
@@ -28,7 +28,49 @@ let list = [
         text:'精彩瞬间'
     }
 ]
-
+let theme = [
+    {
+        type:0,
+        tit:'意大利塞浦路斯+马耳他+西西里岛10晚13天'
+    },
+    {
+        type:0,
+        tit:'意他+西西里晚13天'
+    },
+    {
+        type:0,
+        tit:'意大利塞西西里3天'
+    },
+    {
+        type:1,
+        tit:'意大利7'
+    },
+    {
+        type:1,
+        tit:'意大利6'
+    },
+    {
+        type:2,
+        tit:'意大5'
+    },
+    {
+        type:2,
+        tit:'意2'
+    },
+    {
+        type:2,
+        tit:'塞3'
+    },
+    {
+        type:2,
+        tit:'意大4'
+    },
+]
 app.get('/api/list',(req,res)=>{
-    res.json(list)
+    res.json(list);
+})
+app.get('/api/nav',(req,res)=>{
+    console.log(req.query);
+    let arr = theme.filter((item)=>req.query.id==item.type)
+    res.json(arr);
 })
--- src/containers/Home/subComponents/Nav/index.js
+++ src/containers/Home/subComponents/Nav/index.js
@@ -1,17 +1,27 @@
 import React,{Component} from 'react';
 import './index.less';
+import axios from 'axios';
 let navList = [
 '海岛度假','历史文化','探索冒险','美食购物',
 '都市生活','邮轮度假','海外游学','亲子乐园'
 ]
 export default class Nav extends Component {
+    handleClick = (id)=>{
+        axios.get('/api/nav',{
+            params:{
+                id:id
+            }
+        }).then((res)=>{
+            console.log(res)
+        })
+    }
     render(){
         return(
             <ul className="nav">
                 {
                     navList.map((item,index)=>{
                         return (
-                            <li key={index}>{item}</li>
+                            <li onClick={()=>this.handleClick(index)} key={index}>{item}</li>
                         )
                     })
                 }
