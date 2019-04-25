671aa1999405e139c14182eb05b8bfc4c8640b85
HL1115 <1127570788@qq.com>
2019-04-25 08:34:20 +0800

    redux

---
+++ src/actionTypes/home.js
@@ -0,0 +1,2 @@
+
+export const GET_THEME_TOUR = 'get_theme_tour';
---
+++ src/actions/home.js
@@ -0,0 +1,7 @@
+import {GET_THEME_TOUR} from '../actionTypes/home';
+export const get_theme = (data)=>{
+    return {
+        type:GET_THEME_TOUR,
+        data:data
+    }
+}
